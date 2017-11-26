02b6ee2b65f8ae8664b5783dba0bec8de127d76c
Ridhwaan <ridhwaan.any@gmail.com>
2017-11-25 20:47:44 -0800

    Add DrawerLayout with settings,about,contact

--- app/src/main/java/com/example/home/superprayer/DashboardActivity.java
+++ app/src/main/java/com/example/home/superprayer/DashboardActivity.java
@@ -8,8 +8,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -26,11 +29,17 @@ public class DashboardActivity extends AppCompatActivity {
     private MyViewPagerAdapter myViewPagerAdapter;
 
     private BottomNavigationView mBottomNav;
+    private DrawerLayout mDrawerNav;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+             mToolBar = findViewById(R.id.nav_toolbar);
+             setSupportActionBar(mToolBar);
 
 
 
@@ -39,19 +48,36 @@ public class DashboardActivity extends AppCompatActivity {
             transaction.replace(R.id.fragment_layout_id, currentFragment, "Prayer Times");
             transaction.commit();
 
+
             mBottomNav = findViewById(R.id.bottom_navigation);
             mBottomNav.setOnNavigationItemSelectedListener(listener);
 
+            mDrawerNav = findViewById(R.id.drawer_layout_id);
+            mToggle = new ActionBarDrawerToggle(this,mDrawerNav,R.string.open,R.string.close);
+
+            mDrawerNav.addDrawerListener(mToggle);
+            mToggle.syncState();
+
+
+
+           getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger_menu);
+           getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
         //mRootViewPager =  findViewById(R.id.fragment_view_pager);
      //   myViewPagerAdapter = new MyViewPagerAdapter(getSupportFragmentManager());
      //   mRootViewPager.setAdapter(myViewPagerAdapter);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
 
-
+        return super.onOptionsItemSelected(item);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
